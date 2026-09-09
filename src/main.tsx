fa912e4732dd254dd194d3899d5f5c73b8cdcc02
risheetaagrawal-jpg <risheeta.agrawal@gmail.com>
2026-09-09 11:54:37 +0530

    Lazy-load desktop cursor code

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { installRecoveredInteractionLayer } from './cursor.ts'
 
-installRecoveredInteractionLayer()
+if (window.matchMedia('(hover: hover) and (pointer: fine) and (min-width: 992px)').matches) {
+  void import('./cursor.ts').then(({ installRecoveredInteractionLayer }) => {
+    installRecoveredInteractionLayer()
+  })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
